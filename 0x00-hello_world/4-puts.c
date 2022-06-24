69025996e4800238c56b1c06ad0fa0667ecd23b4
Evans Njeru <evansnjerudev@gmail.com>
2022-06-24 04:47:26 -0700

    Completed 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description:  which prints a message then exits
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
