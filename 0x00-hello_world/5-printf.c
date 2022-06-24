be1e83312dcdb57882642043ffb083ee9552b7fd
Evans Njeru <evansnjerudev@gmail.com>
2022-06-24 07:39:39 -0700

    Completed 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description:  which prints a message then exits
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
