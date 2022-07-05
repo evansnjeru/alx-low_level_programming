d3c1c7195bcd21fd16bef2641935dbcd59583f1f
Evans Njeru <evansnjerudev@gmail.com>
2022-07-05 03:11:43 -0700

    Completed 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - This is the main function
+ * Description:  prints a message then exits
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
