c6e8ce95da6f24d47249481a69aeec603b418919
Evans Njeru <evansnjerudev@gmail.com>
2022-07-05 18:22:04 -0700

    Completed 13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, accu = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			accu += i;
+	}
+	printf("%d\n", accu);
+
+	return (0);
+}
