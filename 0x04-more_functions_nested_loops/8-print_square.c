cd2c9ae96169fe37d81184371fa6d726e27daf16
Evans Njeru <evansnjerudev@gmail.com>
2022-07-07 20:04:14 -0700

    Completed 8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - draws a diagonal line
+ * @size: length of square
+ *
+ * Return: None
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
