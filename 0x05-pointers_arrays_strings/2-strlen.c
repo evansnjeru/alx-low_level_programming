ef4916daea0304f68a97d922104ce45fb323b081
Evans Njeru <evansnjerudev@gmail.com>
2022-07-11 17:57:24 -0700

    Completed 2.This report by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: char pointer
+ *
+ * Return: None
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
