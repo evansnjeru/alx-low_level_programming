630852e8d73f6a92319617ea9a77dc0c287b92e6
Evans Njeru <evansnjerudev@gmail.com>
2022-07-11 18:13:15 -0700

    Completed 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ * @str:  string
+ *
+ * Return: None
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
+void _puts(char *);
 
 #endif /* MAIN_H */
