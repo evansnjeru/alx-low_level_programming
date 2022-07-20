5ab1d981ec3fec12241be8f68780a2a61607bb73
Evans Njeru <evansnjerudev@gmail.com>
2022-07-19 18:13:49 -0700

    Completed 6. Chess is mental torture

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
+
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+void print_chessboard(char (*)[]);
 
 #endif /* MAIN_H */
